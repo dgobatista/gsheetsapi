991451121c7fdbff9bd39d7e4fa7a8d889fafdc0
Diego Batista <dgobatistam@outlook.com>
2024-03-15 15:03:07 -0300

    Update DadosAPI.js

--- DadosAPI.js
+++ DadosAPI.js
@@ -1,22 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // URL da API
     const apiUrl = "#";
-
-    // Elementos HTML onde você deseja exibir os valores
     const investimentoElement = document.getElementById("investimentoValue");
     const lucroElement = document.getElementById("lucroValue");
     const roiElement = document.getElementById("roiValue");
 
-    // Fazer a solicitação AJAX
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Extrair os valores da API
             const investimento = data.saida[1].Investimento;
             const lucro = data.saida[1].Lucro;
             const roi = data.saida[1].ROI;
 
-            // Atualizar os elementos HTML com os valores
             investimentoElement.textContent = investimento;
             lucroElement.textContent = lucro;
             roiElement.textContent = roi;
